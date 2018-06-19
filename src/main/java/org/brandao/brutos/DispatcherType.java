bc03e5e29d8f872d6be0d2f6e4fbe892da663fa6
afonso <afonso.rbn@gmail.com>
2018-06-19 08:55:27 -0300

    #

--- src/main/java/org/brandao/brutos/DispatcherType.java
+++ src/main/java/org/brandao/brutos/DispatcherType.java
@@ -35,10 +35,14 @@ public class DispatcherType {
 		defaultDispatcher.put(FORWARD.toString(),  FORWARD);
 	}
 
-	private String name;
+	protected String name;
 
 	public DispatcherType(String name) {
 		this.name = name;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public String toString() {
@@ -53,6 +57,31 @@ public class DispatcherType {
 			return defaultDispatcher.get(value);
 		else
 			return new DispatcherType(value);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DispatcherType other = (DispatcherType) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 	
 }
